85753b30bcab098b7912bf5d7ee5a1205fe3796c
martinfs1 <martinfernando-sandander@hotmail.com>
2023-01-17 12:18:21 -0300

    add some styles to finish the visual task of the  calculator.

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-console.log('Hello Calculator.');
+console.log("Hello Calculator.");
 
 const add = (a, b) => {
-    return a + b;
-}
-console.log(add(2,3));
+  return a + b;
+};
+console.log(add(2, 3));
 
 const subtract = (a, b) => {
-    return a - b;
-}
-console.log(subtract(3,2));
+  return a - b;
+};
+console.log(subtract(3, 2));
 
 const multiply = (a, b) => {
-    return a * b;
-}
-console.log(multiply(2,3));
+  return a * b;
+};
+console.log(multiply(2, 3));
 
 const divide = (a, b) => {
-    return a / b;
-}
-console.log(divide(10,2));
+  return a / b;
+};
+console.log(divide(10, 2));
 
 const operate = (op, a, b) => {
-    return add(a,b)
-}
-console.log(operate('/', 2,3));
+  return add(a, b);
+};
+console.log(operate("/", 2, 3));
+
+const populateDisplay = () => {};
