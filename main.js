894f381fa669a28ec34deb3b3525e56e832c8f1e
martinfs1 <martinfernando-sandander@hotmail.com>
2023-01-20 16:06:10 -0300

    trying to solve 4 assignment.

--- main.js
+++ main.js
@@ -25,4 +25,62 @@ const operate = (op, a, b) => {
 };
 console.log(operate("/", 2, 3));
 
-const populateDisplay = () => {};
+const numsPad = [];
+for (let i = 0; i < 10; i++) {
+  numsPad.push(i);
+}
+
+// const num0 = document.querySelector("#btnZero");
+// numsPad.push(Number(num0.textContent));
+// num0.addEventListener("click", populateDisplay);
+
+// const num1 = document.querySelector("#btnOne");
+// numsPad.push(Number(num1.textContent));
+// num1.addEventListener("click", populateDisplay);
+
+// const num2 = document.querySelector("#btnTwo");
+// num2.addEventListener("click", populateDisplay);
+// numsPad.push(Number(num2.textContent));
+
+// const num3 = document.querySelector("#btnThree");
+// num3.addEventListener("click", populateDisplay);
+// numsPad.push(Number(num3.textContent));
+
+// const num4 = document.querySelector("#btnFour");
+// num4.addEventListener("click", populateDisplay);
+// numsPad.push(Number(num4.textContent));
+
+// const num5 = document.querySelector("#btnFive");
+// num5.addEventListener("click", populateDisplay);
+// numsPad.push(Number(num5.textContent));
+
+// const num6 = document.querySelector("#btnSix");
+// num6.addEventListener("click", populateDisplay);
+// numsPad.push(Number(num6.textContent));
+
+// const num7 = document.querySelector("#btnSeven");
+// num7.addEventListener("click", populateDisplay);
+// numsPad.push(Number(num7.textContent));
+
+// const num8 = document.querySelector("#btnEight");
+// num8.addEventListener("click", populateDisplay);
+// numsPad.push(Number(num8.textContent));
+
+// const num9 = document.querySelector("#btnNine");
+// num9.addEventListener("click", populateDisplay);
+// numsPad.push(Number(num9.textContent));
+
+console.log(numsPad);
+
+const btnClicked = document.querySelectorAll("button");
+btnClicked.forEach((btn) => {
+  btn.addEventListener("click", console.log(btn.textContent));
+});
+
+function populateDisplay() {
+  const dsply = document.querySelector("#dsply");
+  for (let i = 0; i < 10; index++) {
+    if (i) {
+    }
+  }
+}
