d51f6107609f50b95f1f8a4ca207d801981d7e11
martinfs1 <martinfernando-sandander@hotmail.com>
2023-01-25 16:28:14 -0300

    trying to change if else for swuitch case for improve my addEventListener.

--- main.js
+++ main.js
@@ -27,39 +27,60 @@ console.log(operate("/", 2, 3));
 
 const $dsply = document.querySelector("#dsply");
 function populateDisplay(firstOperands, secondOperands) {
+
 	$dsply.textContent = firstOperands;
 }
-
-// trying to populate the display in order to do a Mathematic operation.
 let firstOperands = []
 let secondOperands = []
 let operator;
+let result
+
+// trying to populate the display in order to do a Mathematic operation.
+
 document.addEventListener('click', (e) => {
 
-	if (e.target.textContent === 'clear') {
-		$dsply.textContent = 0;
-		firstOperands = [];
-		secondOperands = [];
-	} else if ((e.target.textContent === '+')
-		|| (e.target.textContent === '-')
-		|| (e.target.textContent === '*')
-		|| (e.target.textContent === '%')) {
-		operator = e.target.textContent;
-	} else if ((operator) && (firstOperands.length >= 0)) {
+	let btnClicked = e.target.textContent;
 
-		secondOperands.push(Number(e.target.textContent))
-		populateDisplay(secondOperands.join(''));
-		console.log(secondOperands);
+	try {
 
-	} else {
-		firstOperands.push(Number(e.target.textContent))
-		populateDisplay(firstOperands.join(''));
-		console.log(firstOperands);
+		switch (true) {
+			case (btnClicked === 'clear'):
+				$dsply.textContent = 0;
+				firstOperands = [];
+				secondOperands = [];
+				break;
+			case (btnClicked === '+'):
+			case (btnClicked === '-'):
+			case (btnClicked === '*'):
+			case (btnClicked === '/'):
+				operator = e.target.textContent;
+				console.log('operator');
+				break;
+			case (operator) && (firstOperands.length >= 0) && (btnClicked !== '='):
+				$dsply.textContent = "";
+				secondOperands.push(Number(e.target.textContent))
+				populateDisplay(secondOperands.join(''));
+				console.log(secondOperands);
+				break
+			case (btnClicked >= 0 && btnClicked <= 9):
+				firstOperands.push(Number(btnClicked))
+				populateDisplay(firstOperands.join(''));
+				console.log(firstOperands);
+				break
+			case ((btnClicked === '=') && (firstOperands.length >= 0) && (secondOperands.length >= 0)):
+				console.log('Equal');
+				break
+
+		}
+	} catch (error) {
+		console.log(error);
 	}
 
 })
 
+
 // operate(operator, firstOperands, secondOperands)
+
 
 
 
