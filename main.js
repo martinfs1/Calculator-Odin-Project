f763909ca2237d476ed5856991df84c0e0c7b501
martinfs1 <martinfernando-sandander@hotmail.com>
2023-02-03 14:55:51 -0300

    tasks done, the calculator works, not perfectly but show the correct results.

--- main.js
+++ main.js
@@ -1,94 +1,89 @@
 console.log("Hello Calculator.");
 
-const $dsply = document.querySelector("#dsply");
-let firstOperands = []
-let secondOperands = []
-let numbersToOperate1;
-let numbersToOperate2;
+let firstOperands = '';
+let secondOperands = '';
 let operator;
-let result1 = 0;
-;
+let result;
+let interResult;
+let dot
+const $dotBtn = document.querySelector('#btnDot')
+const $dsply = document.querySelector("#dsply");
 
-const add = (a, b) => {
+function add(a, b) {
 	return a + b;
 };
 
-const subtract = (a, b) => {
+function subtract(a, b) {
 	return a - b;
 };
 
-const multiply = (a, b) => {
+function multiply(a, b) {
 	return a * b;
 };
 
-const divide = (a, b) => {
-	return a / b;
+function divide(a, b) {
+	if (b === 0) {
+		return 'Error';
+	} else {
+		return a / b;
+	}
+
 };
 
 const operate = (op, a, b) => {
-	let sign = op;
-	switch (sign) {
+	switch (op) {
 		case '+':
 			return add(Number(a), Number(b));
-			break;
 		case '-':
-			return subtract(a, b)
-			break
+			return subtract(Number(a), Number(b))
 		case 'x':
-			return multiply(a, b)
-			break
+			return multiply(Number(a), Number(b))
 		case '÷':
-			return divide(a, b)
-			break
-		default:
-			break;
+			return divide(Number(a), Number(b))
 	}
 };
 
+function updateDisplay(value) {
+	$dsply.textContent = value;
+}
+
 document.addEventListener('click', (e) => {
-	let btnClicked = e.target.textContent;
-	try {
-		switch (true) {
-			case ((btnClicked === '+') || (btnClicked === '-')
-				|| (btnClicked === 'x') || (btnClicked === '÷')):
-				operator = btnClicked
-				break;
-			case ((btnClicked >= 0) && (btnClicked <= 9) && (!operator)):
-				firstOperands.push(Number(btnClicked))
-				numbersToOperate1 = firstOperands.join('');
-				$dsply.textContent = numbersToOperate1;
-				break
-			case (operator) && (firstOperands.length > 0) && (btnClicked !== '=') && (btnClicked !== 'clear'):
-				$dsply.textContent = " ";
-				secondOperands.push(Number(btnClicked))
-				numbersToOperate2 = secondOperands.join('');
-				$dsply.textContent = numbersToOperate2;
-				break
-			case ((btnClicked === '=') && (numbersToOperate1.length >= 0) && (numbersToOperate2.length >= 0)):
-				$dsply.textContent = "0"
-				result1 = operate(operator, numbersToOperate1, numbersToOperate2)
-				$dsply.textContent = result1;
-				break
-			case (btnClicked === 'clear'):
-				$dsply.textContent = 0;
-				firstOperands = [];
-				secondOperands = [];
-				numbersToOperate1 = ""
-				numbersToOperate2 = ""
-				operator = ""
-				break;
+	const btnClicked = e.target.textContent;
+
+	if (!isNaN(btnClicked) || btnClicked === '.') {
+		if ((!dot) && (btnClicked === '.') && (firstOperands || secondOperands)) {
+			if (operator) {
+				secondOperands += btnClicked;
+			} else {
+				firstOperands += btnClicked;
+			}
+			$dotBtn.setAttribute('disabled', true)
+		} else if (operator) {
+			secondOperands += btnClicked
+
+		} else {
+			firstOperands += btnClicked;
 		}
-	} catch (error) {
-		console.log(error);
+
+	} else if ((btnClicked === '+') || (btnClicked === '-') || (btnClicked === 'x') || (btnClicked === '÷')) {
+		$dotBtn.disabled = false;
+		if (operator) {
+			interResult = operate(operator, firstOperands, secondOperands)
+			firstOperands = interResult;
+			secondOperands = '';
+		}
+		operator = btnClicked;
+	} else if (btnClicked === '=') {
+		result = operate(operator, firstOperands, secondOperands);
+	} else if (btnClicked === 'clear') {
+		firstOperands = '';
+		secondOperands = '';
+		interResult = '';
+		operator = '';
+		result = '';
+		$dotBtn.disabled = false;
+		dot = "";
+
 	}
-
-})
-
-
-
-
-
-
-
-
-
+	updateDisplay(result || secondOperands || firstOperands || interResult || 0)
+});
